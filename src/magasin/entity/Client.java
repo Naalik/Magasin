c47cea0678977bd2abbaabd1bcc5a4e2dbf29474
Naalik <lormir.panther@gmail.com>
2016-09-21 10:15:58 +0200

    commit exo BDD magasin entities

--- src/magasin/entity/Client.java
+++ src/magasin/entity/Client.java
@@ -64,5 +64,53 @@ public class Client implements Serializable {
     public String toString() {
         return "magasin.entity.Client[ id=" + id + " ]";
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
     
 }
